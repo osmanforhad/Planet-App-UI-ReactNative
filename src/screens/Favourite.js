88d7709ca0dbffe3651108c4bba6da8fa8bc11db
osmanforhad <osmanforhad93@gmail.com>
2021-05-28 23:21:04 +0600

    React Native Planet App UI

---
+++ src/screens/Favourite.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Favorite = () => {
+    return(
+        <View style={{
+            justifyContent:"center",
+            alignContent:"center",
+            flex:1,
+        }}>
+            <Text>Favorite</Text>
+        </View>
+    );  
+}
+
+export default Favorite;
